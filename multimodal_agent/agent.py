de1c0f8e65e418428c58fec49df84f3cb7c58f1d
fgh23333 <635262140@qq.com>
2025-09-13 13:36:26 +0800

    添加 react_planner 实例并将其集成到 meta_agent 的配置中

--- multimodal_agent/agent.py
+++ multimodal_agent/agent.py
@@ -10,6 +10,7 @@ from google.adk.models.lite_llm import LiteLlm
 from tools.file_reader_tool import read_files_as_text_callback
 from google.adk.tools.mcp_tool import StreamableHTTPConnectionParams
 from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
+from google.adk.planners.plan_re_act_planner import PlanReActPlanner
 
 # Your ADK agent connects to the remote MCP service via Streamable HTTP
 # mcp_toolset = McpToolset(
@@ -29,6 +30,8 @@ SYSTEM_PROMPT = """
 你的任务是根据用户的需求（可能包含文本、代码和上传的文件），提供专业、准确的回答。
 """
 
+react_planner = PlanReActPlanner()
+
 # --- Agent 实例化 (使用新工具和新提示) ---
 # model_config = LiteLlm(
 #     # 强烈建议使用原生支持多模态的强大模型
@@ -41,6 +44,7 @@ meta_agent = LlmAgent(
     name="MetaAgent",
     model="gemini-2.5-flash",
     instruction=SYSTEM_PROMPT,
+    planner=react_planner,
     # before_model_callback=read_files_as_text_callback,
     # tools=[mcp_toolset]
 )
